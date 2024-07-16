49ab155c4479be59b90a2aed3f887fbba3e932fe
Mudit Gupta <100394697+guptamudit@users.noreply.github.com>
2024-07-16 13:13:08 +0530

    Time: 11 ms (90.58%), Space: 21.8 MB (14.01%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& arr) {
-        vector<int> temp;
-        for(int i=0; i<arr.size();i++){
-            if(arr[i] != 0){
-                temp.push_back(arr[i]);
+        int i = 0;
+        for(int j = 0 ; j < arr.size();j++){
+            if (arr[j] != 0 ){
+                swap(arr[j] ,arr[i]);
+                i++;
             }
         }
-        for(int i = 0 ; i< temp.size(); i++){
-            arr[i] = temp[i];
-        }
-        int nz = temp.size();
-        for(int i = nz; i < arr.size(); i++ ){
-            arr[i] = 0;
-        }
+        
     }
 };
