bad2b954d5ff5ec548ff5644f81a8de2458cb263
Mudit Gupta <100394697+guptamudit@users.noreply.github.com>
2024-07-24 22:45:24 +0530

    Time: 18 ms (9.75%), Space: 8.3 MB (40.77%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversed = 0;
+        int original = x;
+
+        while (x > reversed) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        return x == reversed || x == reversed / 10;
+    }
+};
