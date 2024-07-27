a37752863fc05b8785e60f7a19bfdb8c45549ee4
Mudit Gupta <100394697+guptamudit@users.noreply.github.com>
2024-07-27 11:15:16 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (12.22%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
     int reverse(int x) {
-        stringstream ss;
-        ss << x;
-        string str = ss.str();
-        
-        if(str == "7463847412" || str == "-8463847412"){
-            return(0);
+        int ans = 0;
+        while(x != 0){
+            int digit = x % 10;
+            if((ans > INT_MAX/10)|| (ans < INT_MIN/10)){
+                return 0;
+            }
+            ans = (ans*10) + digit;
+            x = x/10;
+            
         }
-
-        string result = "";
-        bool isNegative = x < 0;
-        int startIndex = isNegative ? 1 : 0;
-
-        if(isNegative){
-            result += "-";
-        }
-
-        for(int i = str.length() - 1; i >= startIndex; i--){
-            result += str[i];
-        }
-        
-        // Check for overflow/underflow
-        try{
-            int reversed = stoi(result);
-            return(reversed);
-        } catch(out_of_range& e) {
-            // Handle overflow/underflow
-            return(0);
-        }
+       return ans;
     }
 };
