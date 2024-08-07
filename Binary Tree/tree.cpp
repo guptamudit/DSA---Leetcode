aaa46839f1cbb89ed89341449285951f32f3a353
Mudit <muditgupta1711@gmail.com>
2024-08-07 21:56:06 +0530

    change

--- Binary Tree/tree.cpp
+++ Binary Tree/tree.cpp
@@ -53,6 +53,34 @@ void postorder(Node *node)
     cout << node->data << endl;
 }
 
+// level order traversal
+vector<vector<int>> levelorder(Node *root)
+{
+    vector<vector<int>> ans;
+    if (root == NULL)
+        return ans;
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        int size = q.size();
+        vector<int> level;
+        for (int i = 0; i < size; i++)
+        {
+            Node *node = q.front();
+            q.pop();
+            if (node->left)
+                q.push(node->left);
+            if (node->right)
+                q.push(node->right);
+            level.push_back(node->data);
+        }
+        ans.push_back(level);
+    }
+    return ans;
+}
+
 int main()
 {
     struct Node *root = new Node(1);
@@ -60,12 +88,30 @@ int main()
     root->right = new Node(5);
     root->left->left = new Node(3);
     root->left->right = new Node(4);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
 
     cout << "preorder" << endl;
     preorder(root);
     cout << endl;
+
+    cout << "inorder" << endl;
     inorder(root);
     cout << endl;
+
+    cout << "postorder" << endl;
     postorder(root);
+    cout << endl;
+
+    cout << "levelorder" << endl;
+    vector<vector<int>> levelOrderResult = levelorder(root);
+    for (const auto &level : levelOrderResult)
+    {
+        for (int val : level)
+        {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
     cout << endl;
 }
