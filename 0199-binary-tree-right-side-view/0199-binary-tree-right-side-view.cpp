4113a608b6bdeda98a8cc5c03f706a8a89db4917
Mudit Gupta <100394697+guptamudit@users.noreply.github.com>
2024-08-31 12:34:21 +0530

    Time: 0 ms (100.00%), Space: 14.3 MB (46.75%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -1,32 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void find(TreeNode* root, int level, vector<int>& ans){
-        if(root == NULL) return;
-        
-        if(level == ans.size()){
-            ans.push_back(root->val);
-        }
-        
-        find(root->right, level + 1, ans);
-        find(root->left, level + 1, ans);
-    }
-public:
     vector<int> rightSideView(TreeNode* root) {
-      
-        vector<int> ans;
-        find(root, 0, ans);
-        return ans;
-        
+        if (!root) {
+            return {};
+        }
+        vector<int> view;
+        queue<TreeNode*> todo;
+        todo.push(root);
+        while (!todo.empty()) {
+            int n = todo.size();
+            for (int i = 0; i < n; i++) {
+                TreeNode* node = todo.front();
+                todo.pop();
+                if (i == n - 1) {
+                    view.push_back(node -> val);
+                }
+                if (node -> left) {
+                    todo.push(node -> left);
+                }
+                if (node -> right) {
+                    todo.push(node -> right);
+                }
+            }
+        }
+        return view;
     }
 };
