a87c6895009fa8ffaf3b85cc001b2c82769ed7c8
Mudit <muditgupta1711@gmail.com>
2024-09-06 23:12:39 +0530

    triplet sum in array

--- Accenture.cpp
+++ Accenture.cpp
@@ -92,28 +92,142 @@
 //     return 0;
 // }
 
+// #include <bits/stdc++.h>
+// #include <algorithm>
+// using namespace std;
+
+// int helper(int num)
+// {
+//     int sum = 0;
+//     while (num > 0)
+//     {
+//         sum = sum + (num % 2);
+//         num = num / 2;
+//     }
+
+//     return sum;
+// }
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+
+//     int ans = helper(n);
+//     cout << ans << endl;
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// #include <algorithm>
+// using namespace std;
+
+// vector<vector<int>> helper(vector<int> &nums, int target)
+// {
+//     vector<vector<int>> ans;
+//     sort(nums.begin(), nums.end());
+//     for (int i = 0; i < nums.size() - 2; i++)
+//     {
+//         if (i > 0 && nums[i] == nums[i - 1])
+//             continue;
+//         for (int j = i + 1; j < nums.size() - 1; j++)
+//         {
+//             if (j > i + 1 && nums[j] == nums[j - 1])
+//                 continue;
+//             for (int k = j + 1; k < nums.size(); k++)
+//             {
+//                 if (k > j + 1 && nums[k] == nums[k - 1])
+//                     continue;
+//                 if (nums[i] + nums[j] + nums[k] == target)
+//                 {
+//                     ans.push_back({nums[i], nums[j], nums[k]});
+//                 }
+//             }
+//         }
+//     }
+//     return ans;
+// }
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int k;
+//     cin >> k;
+//     vector<int> v(n);
+//     for (int i = 0; i < v.size(); i++)
+//     {
+//         cin >> v[i];
+//     }
+
+//     vector<vector<int>> ans = helper(v, k);
+//     for (auto &triplet : ans)
+//     {
+//         for (auto &num : triplet)
+//         {
+//             cout << num << " ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 #include <algorithm>
 using namespace std;
 
-int helper(int num)
+vector<vector<int>> helper(vector<int> &nums, int target)
 {
-    int sum = 0;
-    while (num > 0)
+    vector<vector<int>> ans;
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size() - 2; i++)
     {
-        sum = sum + (num % 2);
-        num = num / 2;
+        if (i > 0 && nums[i] == nums[i - 1])
+            continue;
+        int left = i + 1;
+        int right = nums.size() - 1;
+        while (left < right)
+        {
+            int sum = nums[i] + nums[left] + nums[right];
+            if (sum < target)
+                left++;
+            else if (sum > target)
+                right--;
+            else
+            {
+                ans.push_back({nums[i], nums[left], nums[right]});
+                while (left < right && nums[left] == nums[left + 1])
+                    left++;
+                while (left < right && nums[right] == nums[right - 1])
+                    right--;
+                left++;
+                right--;
+            }
+        }
     }
-
-    return sum;
+    return ans;
 }
 
 int main()
 {
     int n;
     cin >> n;
+    int k;
+    cin >> k;
+    vector<int> v(n);
+    for (int i = 0; i < v.size(); i++)
+    {
+        cin >> v[i];
+    }
 
-    int ans = helper(n);
-    cout << ans << endl;
+    vector<vector<int>> ans = helper(v, k);
+    for (auto &triplet : ans)
+    {
+        for (auto &num : triplet)
+        {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
