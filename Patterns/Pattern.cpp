9d80152c48e486216a593d52957d382dbf6af456
Mudit <muditgupta1711@gmail.com>
2024-09-28 22:30:46 +0530

    commit

---
+++ Patterns/Pattern.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print1(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void print2(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+}
+
+void print3(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+}
+
+void print4(int n)
+{
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void print5(int n)
+{
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int a;
+    cin >> a;
+    print1(a);
+    print2(a);
+    print3(a);
+    print4(a);
+    print5(a);
+}
