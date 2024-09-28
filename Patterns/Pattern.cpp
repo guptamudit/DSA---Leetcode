d735f02df93eef157db78b804222da81770ce8e5
Mudit <muditgupta1711@gmail.com>
2024-09-28 22:45:17 +0530

    commit

--- Patterns/Pattern.cpp
+++ Patterns/Pattern.cpp
@@ -51,12 +51,21 @@ void print4(int n)
 
 void print5(int n)
 {
-    for (int i = n - 1; i >= 0; i--)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j <= i; j++)
+        for (int j = 0; j < n - i - 1; j++)
         {
-            cout << "* ";
+            cout << " ";
         }
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+
         cout << endl;
     }
 }
