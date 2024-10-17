1ee2ce20de0ebd97b96da6abe3ac7ae290064de5
Mudit Gupta <100394697+guptamudit@users.noreply.github.com>
2024-10-17 11:14:26 +0530

    Time: 3 ms (38.18%), Space: 7.9 MB (12.29%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string numstr = to_string(num);
+        string t = numstr;        
+        for(int i=0;i<t.size();i++){
+            for(int j=i+1;j<t.size();j++){
+                swap(t[i],t[j]);
+                int m = stoi(t);
+                num = m > num ? m : num;
+                t = numstr;
+            }
+        }
+        return num;
+    }
+};
